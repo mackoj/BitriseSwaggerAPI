0691716a77cf7c4a8d424714335d4a284f981860
Jeffrey Macko <github.jm@macko.fr>
2019-12-01 22:05:42 +0100

    Add some script to test the lib generation

---
+++ testLib.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+sw_version=$(swift --version | head -n 1 | cut -d ' ' -f 4)
+podspec=$(ls *.podspec)
+pod lib lint "${podspec}" --no-clean --verbose --swift-version="${sw_version}" --use-libraries --allow
